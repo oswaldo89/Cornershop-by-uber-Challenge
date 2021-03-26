e134f4930d6ec18cfdd5f1540c0bb65c3dc06c6d
Oswaldo Gomez <oswaldo.gomez.ext@oxxo.com>
2021-03-25 18:13:23 -0600

    starting with the main screen layout

--- app/src/main/java/com/cornershop/counterstest/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/cornershop/counterstest/presentation/home/HomeActivity.kt
@@ -1,11 +1,18 @@
 package com.cornershop.counterstest.presentation.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cornershop.counterstest.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
--- app/src/main/java/com/cornershop/counterstest/presentation/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/cornershop/counterstest/presentation/welcome/WelcomeActivity.kt
@@ -1,12 +1,23 @@
 package com.cornershop.counterstest.presentation.welcome
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.cornershop.counterstest.R
+import com.cornershop.counterstest.databinding.ActivityWelcomeBinding
+import com.cornershop.counterstest.presentation.home.HomeActivity
 
 class WelcomeActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.welcomeContentLayout.buttonStart.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
