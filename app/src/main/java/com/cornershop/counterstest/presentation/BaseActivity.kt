2cdd2b32bb5bb18b3b877f365301e087b857ef43
Oswaldo Gomez <oswaldo.gomez.ext@oxxo.com>
2021-03-25 20:12:28 -0600

    toolbar is injected with action buttons inside the layout, in a base activity, to be able to reuse it if possible in other activities.

---
+++ app/src/main/java/com/cornershop/counterstest/presentation/BaseActivity.kt
@@ -0,0 +1,36 @@
+package com.cornershop.counterstest.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+import com.cornershop.counterstest.R
+
+
+abstract class BaseActivity<B : ViewBinding> : AppCompatActivity() {
+
+    lateinit var binding: B
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = getViewBinding()
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.root.findViewById(R.id.countersMenuToolbar))
+        supportActionBar?.let {
+            it.hide()
+            it.setHomeAsUpIndicator(R.drawable.ic_close)
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setDisplayShowTitleEnabled(false)
+        }
+    }
+
+    fun showToolbar() {
+        supportActionBar?.show()
+    }
+
+    fun hideToolbar() {
+        supportActionBar?.hide()
+    }
+
+    abstract fun getViewBinding(): B
+
+}
--- app/src/main/java/com/cornershop/counterstest/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/cornershop/counterstest/presentation/home/HomeActivity.kt
@@ -1,18 +1,46 @@
 package com.cornershop.counterstest.presentation.home
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import com.cornershop.counterstest.R
 import com.cornershop.counterstest.databinding.ActivityHomeBinding
+import com.cornershop.counterstest.presentation.BaseActivity
 
-class HomeActivity : AppCompatActivity() {
-
-    private lateinit var binding : ActivityHomeBinding
+class HomeActivity : BaseActivity<ActivityHomeBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                hideToolbar(); true
+            }
+            R.id.action_delete -> {
+                Toast.makeText(this, "click on delete", Toast.LENGTH_LONG).show(); true
+            }
+            R.id.action_share -> {
+                Toast.makeText(this, "click on share", Toast.LENGTH_LONG).show(); true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    fun addCounter(view: View) {
+        showToolbar()
+    }
+
+    override fun getViewBinding(): ActivityHomeBinding = ActivityHomeBinding.inflate(layoutInflater)
 }
